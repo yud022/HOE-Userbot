a2bb30a4c0e37971076fc0b28abf10ff0fed85c6
yud022 <105608302+yud022@users.noreply.github.com>
2022-05-16 04:51:39 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -206,8 +206,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ AYIIN-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/AyiinXdSupport)"
+                    text = "**✘ HOE-USERBOT ERROR REPORT ✘**\n\n"
+                    link = "[Group Support](https://t.me/HoeeSupport)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
